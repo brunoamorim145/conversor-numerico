0ac7fc091a5632967516b95309d231eb75ab4767
brunoamorim145 <brunoamorim145@gmail.com>
2024-02-13 00:15:15 -0300

    criação da pagina home e componente conversor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ConversorComponent } from './components/conversor/conversor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent,
+    ConversorComponent
   ],
   imports: [
     BrowserModule,
